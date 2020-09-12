5df30f58823077aa0454fb857b66e2f4baf4bdce
Romario Gustave <romagustave10@gmail.com>
2020-09-12 07:10:21 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,20 +1,30 @@
 class AuthorsController < ApplicationController
   def show
-    @author = Author.find(params[:id])
-  end
-
-  def new
-  end
-
-  def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
-  end
-
-  private
-
-  def author_params
-    params.permit(:email, :name)
-  end
+     @author = Author.find(params[:id])
+   end
+ 
+   def new
+ 
+   end
+ 
+   def create
+     @author = Author.new(author_params)
+ 
+     if @author.valid?
+ 
+       @author.save
+       redirect_to author_path(@author)
+ 
+     else
+       
+       render:new
+     end
+ 
+   end
+ 
+   private
+ 
+   def author_params
+     params.permit(:email, :name)
+   end
 end
